9babef36fb7b45c78b07679791af757fd4078f1b
Olga Maunsell <olgamaunsell@yahoo.co.uk>
2018-01-23 16:43:45 +0000

    Initial commit

---
+++ app/src/main/java/example/codeclan/com/stuffforsale/StuffForSaleActivity.java
@@ -0,0 +1,13 @@
+package example.codeclan.com.stuffforsale;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StuffForSaleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.stuff_for_sale_activity);
+    }
+}
